f0eeecb8833cb29734d8193cb855cc4ac6b1e2ee
Chen Zhan <chen.zhan@adelaide.edu.au>
2025-09-02 22:36:37 +0930

    resovle R CMD test warnings

--- R/se_airway.R
+++ R/se_airway.R
@@ -1,11 +1,16 @@
-#' Example SummarizedExperiment Dataset
+#' Example SummarizedExperiment dataset
 #'
-#' This dataset contains scaled RNA-seq counts for airway samples.
+#' Scaled RNA-seq counts for airway samples packaged as a
+#' \link[SummarizedExperiment]{SummarizedExperiment}.
 #'
+#' @docType data
+#' @name se_airway
+#' @usage data(se_airway)
 #' @format A SummarizedExperiment object with:
 #' \describe{
 #'   \item{assays}{A matrix of scaled counts}
 #'   \item{colData}{Sample metadata (cell type, treatment, etc.)}
 #' }
-#' @source Airway dataset
+#' @keywords datasets
+#' @source airway package
 "se_airway"
